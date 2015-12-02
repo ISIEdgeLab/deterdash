08aea5f08bd01a3b755cc54f835c7b038ea95a01
Geoff Lawler <glawler@tislabs.com>
2015-12-02 14:57:39 -0800

    PI meeting udpates. removed height/extent GUI bits. widget auto-extents now. this is temporary.

--- deterdash/viz_data.py
+++ deterdash/viz_data.py
@@ -6,45 +6,6 @@ from magi.testbed import testbed
 
 log = logging.getLogger(__name__)
 
-def get_viz_data(start, stop, step, node, metric, agent, datatype):
-    db = magi_db()
-    data = []
-
-    # prefill with zeros. fill in actual data from the DB.
-    data = {t: 0.0 for t in xrange(start, stop, step)}
-
-    # find the correct table, column, and data entry to query.
-    viz_data_entry = db.experiment_data.find_one({
-        'agent': 'viz_data',
-        'datatype': datatype,
-        'table': agent,
-        'units': {'$elemMatch': {'data_key': metric}}
-    })
-
-    if not viz_data_entry:
-        msg = 'No such data type in the viz_data table: {}/{}/{}'.format(
-            datatype, agent, metric)
-        log.warn(msg)
-        return None
-
-    cursor = db.experiment_data.find(
-        {
-            'agent': viz_data_entry['table'],  # Magi stores the "table name" in the "agent" field.
-            'created': {'$gte': float(start), '$lt': float(stop)},
-            viz_data_entry['node_key']: node,
-        }
-    )
-    cursor.sort([('created', 1)])
-
-    # now we have sorted data. We need to match the requested time slots with
-    # the requested data. The DB data may be sparse.
-    if cursor.alive:
-        rows = list(cursor)
-        for row in rows:
-            data[int(row['created'])] = row[metric]
-
-    return data.values()
-
 def load_default_viz_ui_types():
     '''Load the standard supported graph types and instances. These only come from
     the deafult Magi-loaded agents. It also loads (creates) the offically supported
