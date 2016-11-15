abb9ee748a3cfbb80b7ba973ae340bf468c4b580
Geoff Lawler <glawler@tislabs.com>
2016-11-15 12:46:47 -0800

    Updated topology typoe handling. Now can specify an implementation to use (html file), and "extra keys" so the server can find the data to display in the database. It is not always enough to just search by agent name...

--- lib/libdeterdash/libdeterdash.py
+++ lib/libdeterdash/libdeterdash.py
@@ -8,12 +8,12 @@
 #
 import logging
 import time
+import pymongo
 
 from magi.util import database
 from magi.testbed import testbed
 
 log = logging.getLogger(__name__)
-
 
 class DeterDashboard(object):
 
@@ -54,22 +54,31 @@ class DeterDashboard(object):
 
         return True
 
-    def add_topology(self, display, table, node_key, edges_key):
-        collection = database.getCollection(DeterDashboard.viz_data_table)
-        collection.update(
-            {
-                'agent': DeterDashboard.viz_data_table,
-                'host': testbed.nodename
-            },
-            {
-                'host': testbed.nodename,
-                'created': time.time(),
-                'agent': DeterDashboard.viz_data_table,
-                'datatype': DeterDashboard.force_directed_graph_type,
-                'display': display,
-                'table': table,
-                'node_key': node_key,
-                'data_key': edges_key
-            },
-            upsert=True
-        )
+    def add_topology(self, display, table, node_key, edges_key, template=None, extra_keys=None):
+        '''Add a topology to the GUI. table_keys is a list of keys which identify the table.
+        node_key and edges_key identify the nodes and edges data.'''
+        template = 'force_graph.html' if not template else template
+        viz_collection = database.getCollection(DeterDashboard.viz_data_table)
+        viz_collection.insert({
+            'datatype': DeterDashboard.force_directed_graph_type,
+            'display': display,
+            'table': table,
+            'node_key': node_key,
+            'edges_key': edges_key,
+            'extra_keys': extra_keys,
+            'template': template
+        })
+
+
+    def get_topology_keys(self, agent):
+        '''Given the agent, return the keys needed to extract the nodes and edges from the DB.'''
+        viz_collection = database.getCollection(DeterDashboard.viz_data_table)
+        cursor = viz_collection.find({
+            'datatype': DeterDashboard.force_directed_graph_type,
+            'table': agent
+        }).sort('created', pymongo.DESCENDING).limit(1)
+
+        if cursor.count():
+            return cursor[0]['node_key'], cursor[0]['edges_key'], cursor[0]['extra_keys']
+
+        return None, None, None
