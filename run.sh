92933f8a55ec2542e841812f730c30f36190a9d3
Geoff Lawler <glawler@tislabs.com>
2016-11-15 13:00:45 -0800

    a simple script that starts the server after installing required packages. assumes apt-get.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+for p in python-flask python-pymongo; do
+    if [ $(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+        apt-get install -y ${p}
+    fi
+done
+
+if [[ ! -e /usr/local/lib/python2.7/dist-packages/libdeterdash-0.1.egg-info ]]; then 
+    sudo /proj/edgect/magi/current/source/libdeterdash.install /tmp/ /proj/edgect/magi/current/source
+fi
+
+# now actually run it.
+./runserver.py -l debug
