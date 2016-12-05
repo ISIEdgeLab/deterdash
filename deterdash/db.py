f137c4c9e6c9dbcf989e94bbc1d23040cce6d851
Geoff Lawler <glawler@tislabs.com>
2016-12-05 13:31:35 -0800

    read db port from experiment.conf. Also wait until we have a database connection before doing anything. There is now a sleep loop of 5 seconds until a connection can be established. This will help in daemonized mode as deterdash will not bail out but wait until Magi is running.

--- deterdash/db.py
+++ deterdash/db.py
@@ -1,6 +1,9 @@
 import logging
 import yaml
+from time import sleep
+
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from flask import g
 
 log = logging.getLogger(__name__)
@@ -10,45 +13,60 @@ magi_db_name_no_namespace_clash = None
 def db_server():
     global magi_db_name_no_namespace_clash
     name = getattr(g, 'magi_db_servername', None)
+    port = getattr(g, 'magi_db_port', None)
     if not name:
         if magi_db_name_no_namespace_clash:
-           g.magi_db_servername = name = magi_db_name_no_namespace_clash
-           return name
+           g.magi_db_servername = name = magi_db_name_no_namespace_clash[0]
+           g.magi_db_port = port = magi_db_name_no_namespace_clash[1]
+           return name, port
 
         with open('/var/log/magi/config/experiment.conf', 'r') as fd:
             expconf = yaml.safe_load(fd)
 
         if 'dbdl' not in expconf:
-            log.critical('did not file database config in experiment.conf')
+            log.critical('did not find database config in experiment.conf')
             exit(1)
 
         if 'configHost' in expconf['dbdl']:
             g.magi_db_servername = name = expconf['dbdl']['configHost']
-            magi_db_name_no_namespace_clash = name
+            g.magi_db_port = port = expconf['dbdl']['collectorPort']
+            magi_db_name_no_namespace_clash = (name, port)
         elif 'sensorToCollectorMap' in expconf['dbdl']:
             if '__DEFAULT__' not in expconf['dbdl']['sensorToCollectorMap']:
                 log.critical('no default collector in experiment.conf')
                 exit(2)
             else:
                 g.magi_db_servername = name = expconf['dbdl']['sensorToCollectorMap']['__DEFAULT__']
-                magi_db_name_no_namespace_clash = name
+                g.magi_db_port = port = expconf['dbdl']['collectorPort']
+                magi_db_name_no_namespace_clash = (name, port)
         else:
             log.critical('Unable to find database server in experiment.conf')
             exit(3)
 
         # log.info('Found database server name: {}'.format(name))
 
-    return name
+    return name, port
 
 def magi_db():
     db = getattr(g, 'magi_db_client', None)
     if not db:
-        server_name = db_server()
-        client = MongoClient(server_name, 27018)
-        if not client:
-            log.critical('Unable to contact db server on {}.'.format(server_name))
-            return None
+        server_name, server_port = db_server()
+        client = None
+        while True:
+            try: 
+                client = MongoClient(server_name, server_port)
+            except ConnectionFailure as e:
+                log.info('Unable to connect to the db: {}'.format(e))
+                log.info('Sleeping for awhile and trying again.')
+                sleep(5)
+                continue
 
+            if not client:
+                log.critical('Unable to contact db server on {}.'.format(server_name))
+                return None
+
+            break   # We've got a database. We can continue.
+        
         g.magi_db_client = db = client['magi']
 
     return db
